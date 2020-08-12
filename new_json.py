289c115cf5ec178e8a961aa026fc5fd6cfc1ecf0
My Name <tcshcc07@gmail.com>
2020-08-12 22:33:29 +0500

    Корректировка кода для более простого вывода решения.

--- new_json.py
+++ new_json.py
@@ -1,27 +1,19 @@
 import json
-from collections import defaultdict
 from collections import Counter
-
-counter = defaultdict(int)
 
 with open('newsafr.json', encoding='utf-8') as f:
     file_json = json.load(f)
 
+list_elem = []
 for key in file_json['rss']['channel']['items']:
     value = key['description'].split()
     for elem in value:
-        counter[elem] += 1
-        dups = {e: count for e, count in counter.items()}
+        list_elem.append(elem)
 
-sorted_keys = sorted(dups, key=dups.get, reverse=True)
+dictionary_news = {}
+for key_words, count_repetitions in dict(Counter(sorted(list_elem))).items():
+    if len(key_words) > 6:
+        dictionary_news[key_words] = count_repetitions
 
-dictionary = {}
-
-for keys in sorted_keys:
-    if len(keys) > 6:
-        dictionary[keys] = dups[keys]
-
-vocabulary_list = Counter.most_common(dictionary, 10)
-
-for final_value in vocabulary_list:
-    print(f'Слово "{final_value[0]}" повторяется {final_value[1]} раз')
+for x in Counter.most_common(dictionary_news, 10):
+    print(f'Слово "{list(x)[0]}" повторяется {list(x)[1]} раз')
